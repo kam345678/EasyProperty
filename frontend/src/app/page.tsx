60456ae1af1383b2556de5033c3fa9057f060d0d
kam345678 <tk.kam23132@gmail.com>
2026-02-21 21:03:01 +0700

    edite layout and package.json

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client"
 
 import Image from "next/image"
@@ -175,13 +174,4 @@ function Button({ text }: any) {
       {text}
     </button>
   )
-=======
-import "../styles/globals.css";
-export default function HomePage() {
-    return (   
-       <div className="bg-gradient-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 via-indigo-500 to-violet-500">
-            <h1 className="text-2xl font-bold text-white">Welcome to the Home Page</h1>
-       </div>
-    );
->>>>>>> 756f3ace5007c52251664ca907f92b20e68a6cb3
 }
