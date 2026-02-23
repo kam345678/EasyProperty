441ce2864c54d16f0e70f865e206d1d06a3cdd7c
kam345678 <tk.kam23132@gmail.com>
2026-02-23 16:59:11 +0700

    frontend/src/app/admin/งานซ่อม/page.tsx edite by kam

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-import "../styles/globals.css";
-
-export const metadata = {
-  title: "EasyApartment",
-  description: "ระบบจัดการห้องพัก",
-};
-=======
 import "../styles/globals.css"
 import { AuthProvider } from "@/context/AuthContext"
->>>>>>> f08fcb8e40c3d5fd6275bde0ff62fa9c97b1daea
 
 export default function RootLayout({
   children,
