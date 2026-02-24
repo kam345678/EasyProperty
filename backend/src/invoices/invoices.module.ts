e8798989020b59800eaf3799854b9e4814f444bd
kam345678 <tk.kam23132@gmail.com>
2026-02-24 23:25:21 +0700

    kajdf

--- backend/src/invoices/invoices.module.ts
+++ backend/src/invoices/invoices.module.ts
@@ -2,10 +2,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Invoice, InvoiceSchema } from './schemas/invoice.schema';
+import { Contract, ContractSchema } from '../contracts/schemas/contract.schema';
+import { Room, RoomSchema } from '.././rooms/schema/room.schema';
+import { InvoicesController } from './invoices.controller';
+import { InvoicesService } from './invoices.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Invoice.name, schema: InvoiceSchema }]),
+    MongooseModule.forFeature([
+      { name: Invoice.name, schema: InvoiceSchema },
+      { name: Contract.name, schema: ContractSchema },
+      { name: Room.name, schema: RoomSchema },
+    ]),
   ],
+  controllers: [InvoicesController],
+  providers: [InvoicesService],
 })
 export class InvoicesModule {}
